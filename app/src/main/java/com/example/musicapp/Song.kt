e8ce5a2dcff704de482c8c36bfcf889e6c4b977b
Giovetty <kelsia20giovetty@gmail.com>
2026-04-17 15:21:19 +0100

    Initialise the  variable for the song objects

--- app/src/main/java/com/example/musicapp/Song.kt
+++ app/src/main/java/com/example/musicapp/Song.kt
@@ -2,12 +2,21 @@ package com.example.musicapp
 
 import android.util.Log
 
-class Song {
+class Song (SongName: String, SongArtist: String, SongYear: Int, SongDuration: String,){
+
     // adding variables for song object's attributes
     var name : String = ""
     var artist : String = ""
     var year : Int = 0
     var duration : String = ""
+
+    // initialise the  variable/attributes for the song objects
+    init {
+        name = SongName
+        artist = SongArtist
+        year = SongYear
+        duration = SongDuration
+    }
 
     //function to play a song
     fun Play(){
