068781d4da5ab39c074b31c86450e1885e12c6ce
Giovetty <kelsia20giovetty@gmail.com>
2026-04-17 15:47:23 +0100

    Calling the song class

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -11,6 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        Song("So Nos dois", "Johnny Ramos",2009,"4:35")
+        Song("Bam Bam", "HUGEL",2025,"2:37")
+        Song("Easy to Love","Bucie")
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
